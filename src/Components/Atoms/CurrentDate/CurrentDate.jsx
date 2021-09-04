c8736f2844891f5078476fbf14b179d9974974ca
MinsangKwak <kmsdevwork@gmail.com>
2021-09-04 22:24:37 +0900

    [feat] Add CurrentDate
    CurrentDate 컴포넌트 추가

---
+++ src/Components/Atoms/CurrentDate/CurrentDate.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CurrentDateStyle } from "./CurrentDateStyle";
+
+const Date = ({ date }) => {
+    return <CurrentDateStyle>{date}</CurrentDateStyle>;
+};
+
+export default Date;
