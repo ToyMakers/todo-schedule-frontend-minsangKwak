ad4bc394b2b2ebca870d3a464e210afc1466826a
MinsangKwak <kmsdevwork@gmail.com>
2021-09-04 22:53:00 +0900

    [fix] Fix App
    App 컴포넌트 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,12 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./Components/Styles/Theme";
 import GlobalStyle from "./Components/Styles/GlobalStyle";
-// import Header from "./Components/Layout/Header";
-// import Section from "./Components/Layout/Section";
-// import TodoTemplate from "./Components/Todo/TodoTemplate";
-// import TodoHead from "./Components/Todo/TodoHead";
-// import TodoCreate from "./Components/Todo/TodoCreate";
-// import TodoItem from "./Components/Todo/TodoList";
 import Todo from "./Components/Pages/Todo/Todo";
 
 const App = () => {
     return (
         <div>
             <ThemeProvider theme={theme}>
-                {/* <Header />
-                <Section>
-                    <TodoTemplate>
-                        <TodoHead />
-                        <TodoItem />
-                        <TodoCreate />
-                    </TodoTemplate>
-                </Section> */}
                 <Todo />
                 <GlobalStyle />
             </ThemeProvider>
